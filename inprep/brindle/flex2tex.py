1e1293331a8f6a412be860da3c2500fcc800fe9c
glottotopia <sebastian.nordhoff@glottotopia.de>
2015-01-23 16:03:40 +0100

    original with all classes runs without errors

--- inprep/brindle/flex2tex.py
+++ inprep/brindle/flex2tex.py
@@ -5,87 +5,104 @@ import xml.etree.ElementTree as etree
 import re
 
 def cmd(c,v, indent=0):
-  return indent*u' '+u"\\%s{%s}"%(c,v) 
+    return indent*u' '+u"\\%s{%s}"%(c,v) 
+
+def getText(e,field,strtype):
+    try:
+	return e.find('%s/%s/Run'%(field,strtype)).text
+    except AttributeError:
+	return False
+      
 
 def label2latex(s):
-  return "\\hypertarget{%s}{}" % s
+    return "\\hypertarget{%s}{}" % s
 
 class LexEntry():
   
-  def __init__(self,e):
-    self.headword = Headword(e.find('LexEntry_HeadWord'))
-    self.label = e.attrib['id']
-    try:
-      self.pronunciations = [Pronunciation(p) for p in  e.find('LexEntry_Pronunciations').findall('LexPronunciation')]
-    except AttributeError:
-      #print("no pronunciation for {}".format(e.attrib["id"]))
-      self.pronunciations = []
-    self.pos = POS(e.find('MoStemMsa'))
-    self.senses =  [Sense(s) for s in e.find('LexEntry_Senses').findall('LexSense')]
-  
-  def toLatex(self): 
-    self.headword.toLatex()
-    print label2latex(self.label)
-    if len(self.pronunciations) == 0:
-      print '{\\fixpron}'
-    for p in self.pronunciations:
-      p.toLatex()
-    self.pos.toLatex()
-    if len(self.senses)==1:
-      self.senses[0].toLatex()
-    else:
-      for i,s in enumerate(self.senses):
-	s.toLatex(number=i+1)
+    def __init__(self,e):
+	self.label = e.attrib['id']
+	self.etymology = Etymology(e.find('../LexEtymology'))
+	self.headword = Headword(e.find('LexEntry_HeadWord'))
+	self.literalmeaning = getText(e,'LexEntry_LiteralMeaning','AStr')
+	try:
+	    self.pronunciations = [Pronunciation(p) for p in  e.find('LexEntry_Pronunciations').findall('LexPronunciation')]
+	except AttributeError:
+	    #print("no pronunciation for {}".format(e.attrib["id"]))
+	    self.pronunciations = []
+	try:
+	    self.mlr = MimimalLexReferences(e.find('MimimalLexReferences'))
+	except AttributeError:
+	    pass
+	try:
+	    self.vfebr = VariantFormEntryBackRefs(e.find('VariantFormEntryBackRefs'))
+	except AttributeError:
+	    pass
+	self.pos = POS(e.find('MoStemMsa'))
+	self.senses =  [Sense(s) for s in e.find('LexEntry_Senses').findall('LexSense')]
+	self.plural = getText(e,'LexEntry_plural_form','AStr')
+    
+    def toLatex(self): 
+	self.headword.toLatex()
+	print label2latex(self.label)
+	if len(self.pronunciations) == 0:
+	    print '{\\fixpron}'
+	for p in self.pronunciations:
+	    p.toLatex()
+	self.pos.toLatex()
+	if len(self.senses)==1:
+	    self.senses[0].toLatex()
+	else:
+	    for i,s in enumerate(self.senses):
+		s.toLatex(number=i+1)
 
 class Headword():
-  def __init__(self,e):
-    self.word = e.findall('.//Run')[0].text 
-    try:
-      self.homograph = e.findall('.//Run')[1].text 
-
-    except IndexError:
-      self.homograph = False
-  
-  def toLatex(self):
-    print "\\newentry"
-    if self.homograph:
-      print "".join([cmd('homograph',self.homograph), cmd('headword',self.word)]).encode('utf-8') 
-    else:
-      print cmd('headword',self.word).encode('utf-8') 
+    def __init__(self,e):
+	self.word = e.findall('.//Run')[0].text 
+	try:
+	    self.homograph = e.findall('.//Run')[1].text #better use attrib named style
+	except IndexError:
+	    self.homograph = False
+      
+    def toLatex(self):
+	print "\\newentry"
+	if self.homograph:
+	    print "".join([cmd('homograph',self.homograph), cmd('headword',self.word)]).encode('utf-8') 
+	else:
+	    print cmd('headword',self.word).encode('utf-8') 
 
     
 class POS():
-  def __init__(self,p): 
-    try:
-      self.pos = p.find('.//Run').text 
-    except AttributeError:
-      self.pos = '{\\fixpos}'
-    
-    
-  def toLatex(self): 
-    print cmd('pos',self.pos, indent=1).encode('utf-8')
+    def __init__(self,p): 
+	try:
+	    self.pos = p.find('.//Run').text 
+	except AttributeError:
+	    self.pos = '{\\fixpos}'
+	
+    def toLatex(self): 
+	print cmd('pos',self.pos, indent=1).encode('utf-8')
   
     
 class Pronunciation():
-  def __init__(self,p): 
-    self.ipa = p.find('.//Run').text 
+    def __init__(self,p): 
+	self.ipa = p.find('.//Run').text 
+      
+    def toLatex(self): 
+	print cmd('ipa',self.ipa, indent=1).encode('utf-8')
     
-  def toLatex(self): 
-    print cmd('ipa',self.ipa, indent=1).encode('utf-8')
-  
 class Sense():
   def __init__(self,s):
+    self.definition = getText(s,'LexSense_Definition','AStr')
+    self.examples = [Example(x) for x in s.findall('.//LexExampleSentence')]     
+    self.references = [LexReflink(l) for l in (s.findall('.//LexReferenceLink'))]
+    self.scientificname = getText(s,'LexSense_ScientificName','Str')
+    self.usagetypes = [a.attrib['name'] for a in (s.findall('LexSense_UsageTypes/Link/Alt'))]
     try:
-      self.synpos = s.find('.//MoMorphSynAnalysisLink_MLPartOfSpeech').find('.//Run').text 
-    except AttributeError:	
-      #print("no pos for {}".format(s.attrib["id"]))
-      self.synpos = '{\\fixpos}'
-    try:
-      self.definition = s.find('.//LexSense_Definition').find('.//Run').text 
-    except AttributeError:
-      #print("no definition for {}".format(s.attrib["id"]))      
-      self.definition = '{\\fixdef}'
-    self.examples = [Example(x) for x in s.findall('.//LexExampleSentence')] 
+      self.lsveferbs = [Veferbs(l) for l in (s.find('LexEntryRef_VariantEntryTypes'))]
+    except TypeError:
+      pass
+    self.lfg = getText(s,'LexSense_lexical_function_glosses','Str')
+    self.synpos = getText(s,'MoMorphSynAnalysisLink_MLPartOfSpeech','AStr')
+    self.lsgloss = getText(s,'LexSense_Gloss','AStr') 
   
   def toLatex(self,number=False):
     if number:
@@ -129,10 +146,50 @@ class Translation():
     print cmd('trs',self.string,indent=6).encode('utf-8')
       
 
-
-
-
+class Etymology ():
+      def __init__(self,e):
+	self.form = getText(e,'LexEtymology_Form','AStr')
+	self.gloss = getText(e,'LexEtymology_Gloss','AStr')
+	self.source = getText(e,'LexEtymology_Source','AUni')
   
+      def toLatex(self):
+	  print cmd('etymology','',indent=6).encode('utf-8')
+	  print cmd('etymologyform',self.form,indent=8).encode('utf-8')
+	  print cmd('etymologygloss',self.gloss,indent=8).encode('utf-8')
+	  print cmd('etymologysrc',self.source,indent=8).encode('utf-8')
+	  
+
+class MimimalLexReferences():
+      def __init__(self,e):
+	  self.lexreflinks = [LexReflink(lrl) for lrl in e.findall('LexReferenceLink')]
+      def toLatex(self):
+	  pass
+	
+class LexReflink():
+    def __init__(self,e):
+	  self.type_ = e.find('LexReferenceLink_Type/Link/Alt').attrib.get('abbr')
+	  self.targets = [(l.attrib['target'],l.find('Alt').attrib.get('sense')) for l in e.findall('LexReferenceLink_Targets/Link')]
+  
+
+class VariantFormEntryBackRefs ():
+      def __init__(self,e):
+	  self.lexentryreflinks = [LexEntryReflink(lerl) for lerl in e.findall('LexEntryRefLink')]
+      def toLatex(self):
+	  pass
+	
+	
+class LexEntryReflink():
+    def __init__(self,e):
+	  self.target = e.find('LexEntryRefLink_OwningEntry/Link').attrib['target']
+	  self.alt = e.find('LexEntryRefLink_OwningEntry/Link/Alt').attrib['entry']
+	  self.vet = e.find('LexEntryRefLink_VariantEntryTypes/Link/Alt').attrib['revabbr']
+	  
+    def toLatex(self):
+	  pass
+	
+	 
+
+#===================
 
 fn = sys.argv[1]
 tree = etree.parse(fn)
@@ -147,36 +204,5 @@ for entry in root.findall('LexEntry'):
 for le in lexentries:
   print "%"+30*"-"
   le.toLatex()
-
-<CmTranslation_Translation>                                                                                                                                                         
-<ExportedDictionary>                                                                                                                                                                
-<LexEntry_Etymology>                                                                                                                                                                
-<LexEntry_HeadWord>                                                                                                                                                                 
-<LexEntry_LiteralMeaning>                                                                                                                                                           
-<LexEntry_Pronunciations>                                                                                                                                                           
-<LexEntryRefLink_OwningEntry>                                                                                                                                                       
-<LexEntryRef_VariantEntryTypes>                                                                                                                                                     
-<LexEntry_Senses>
-<LexEtymology>
-<LexEtymology_Form>
-<LexEtymology_Gloss>
-<LexEtymology_Source>
-<LexExampleSentence_Example>
-<LexExampleSentence_Translations>
-<LexPronunciation_Form>
-<LexReferenceLink>
-<LexReferenceLink_Type>
-<LexReference_Targets>
-<LexSense_Definition>
-<LexSense_Examples>
-<LexSense_Gloss>
-<LexSense_LexSenseReferences>
-<LexSense_ScientificName>
-<LexSense_UsageTypes>
-<LexSense_VariantFormEntryBackRefs>
-<Link>
-<MoMorphSynAnalysisLink_MLPartOfSpeech>
-<MoStemMsa_MLPartOfSpeech>
-<Str>
 
   
